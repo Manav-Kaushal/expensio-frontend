00a981fd11200581c32d25ef75b03e58bbbeede8
Manav Kaushal <manavkaushal756@gmail.com>
2023-02-15 03:06:11 +0530

    updated api base url for production

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -3,7 +3,7 @@ export const isProduction = process.env.NEXT_PUBLIC_NODE_ENV === "production";
 export const page = {
   appName: "Expensio",
   apiBaseUrl: isProduction
-    ? "http://localhost:5000/api"
+    ? "https://expensio-backend.vercel.app/api"
     : "http://localhost:5000/api",
 };
 
